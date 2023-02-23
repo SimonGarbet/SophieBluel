6066ec801ca26b2d8933729ad8fbacfe14df054c
SimonG <simon.garbet@gmail.com>
2023-02-23 18:12:00 +0100

    Filtres générés

--- FrontEnd/images.js
+++ FrontEnd/images.js
@@ -1,7 +1,8 @@
 const reponse = await fetch('http://localhost:5678/api/works');
 const works = await reponse.json();
 
-console.log(works);
+
+// Début création d'images à partir de l'API
 
 function genererImages(x){
 
@@ -27,3 +28,60 @@ function genererImages(x){
 
 
 genererImages(works);
+
+
+
+//Fin création d'images à partir de l'API
+
+
+// Début Programmation Bouton
+
+
+const boutonTous= document.querySelector(".btn-tous");
+
+boutonTous.addEventListener("click", function () {
+    document.querySelector(".gallery").innerHTML = "";
+    genererImages(works);
+});
+
+
+const boutonObjets= document.querySelector(".btn-objets");
+
+boutonObjets.addEventListener("click", function () {
+    const objetsFiltres = works.filter(function(work) {
+        return work.category.name === "Objets"
+    });
+
+    document.querySelector(".gallery").innerHTML = "";
+    genererImages(objetsFiltres);
+});
+
+const boutonAppart= document.querySelector(".btn-appart");
+
+boutonAppart.addEventListener("click", function () {
+    const AppartFiltres = works.filter(function(work) {
+        return work.category.name === "Appartements"
+    });
+
+    document.querySelector(".gallery").innerHTML = "";
+    genererImages(AppartFiltres);
+});
+
+
+
+const boutonHotels= document.querySelector(".btn-hotels");
+
+boutonHotels.addEventListener("click", function () {
+    const HotelsFiltres = works.filter(function(work) {
+        return work.category.name === "Hotels & restaurants"
+    });
+
+    document.querySelector(".gallery").innerHTML = "";
+    genererImages(HotelsFiltres);
+});
+
+
+
+// Fin Programmation Boutons
+
+
